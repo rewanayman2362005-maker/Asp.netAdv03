adac62781768d3d41f6c9b7cad20b5f658be0dc3
rewanayman2362005 <rewanayman2362005@gmail.com>
2026-08-29 23:28:48 +0300

    Execrise Q2

--- ADV3/Program.cs
+++ ADV3/Program.cs
@@ -8,6 +8,20 @@
             //List<int> grades = new List<int>() { 85, 92, 78, 95, 88, 70, 100, 65 };
 
             #endregion
+            #region 2. Print the collection, Count, first and last grade
+            //List<int> grades = new List<int>() { 85, 92, 78, 95, 88, 70, 100, 65 };
+            //foreach (int grade in grades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+            //Console.WriteLine($"count :{grades.Count()}");
+            //Console.WriteLine($"first : {grades.First()}");
+            //Console.WriteLine($"last : {grades.Last()}");
+
+
+
+            #endregion
+
 
         }
     }
