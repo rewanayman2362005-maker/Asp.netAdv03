3ab330baa5c927e3fa4cf762e2927547fb2cbad3
rewanayman2362005 <rewanayman2362005@gmail.com>
2026-08-29 23:27:27 +0300

    EXercise Q1

--- ADV3/Program.cs
+++ ADV3/Program.cs
@@ -4,6 +4,11 @@
     {
         static void Main(string[] args)
         {
+            #region  1.Create a Collection with these grades: 85, 92, 78, 95, 88, 70, 100, 65
+            //List<int> grades = new List<int>() { 85, 92, 78, 95, 88, 70, 100, 65 };
+
+            #endregion
+
         }
     }
 }
